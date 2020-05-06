89152bcc2bb12a00903742f88c23193e24113cd2
JosephRArsany <jnhr48@durham.ac.uk>
2020-05-06 10:31:45 +0100

    added npm and updated server.ks

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const http = require('http');
+/*const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 8000;
+const port = 8080;
 
 const server = http.createServer((req, res) => {
-        res.statusCode = 404;
+        res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
         console.log('Server running at http://' + hostname + ':' + port  + '/');
-});
+});*/
+
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, resp){
+   resp.send('Hello world')
+})
+
+app.listen(8080, () => {console.log("Server running at https://127.0.0.1:8080");})

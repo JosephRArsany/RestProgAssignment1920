066c95af7463b14fff598a3ec64c98a286f6f03b
JosephRArsany <jnhr48@durham.ac.uk>
2020-05-06 18:18:09 +0100

    fixed typo

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-/*const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-        console.log('Server running at http://' + hostname + ':' + port  + '/');
-});*/
-
 var express = require('express')
 var app = express()
 
@@ -20,7 +5,7 @@ var birds = require('./birds.json')
 
 
 app.get('/', function(req, resp){
-   resp.send('Bird Database')
+   resp.send('Bird Database');
 })
 
 app.get('/birds/:id/prev', function(req, resp){ 
